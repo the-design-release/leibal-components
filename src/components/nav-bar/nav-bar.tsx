f7403b732b17a383341905972c520ecf82c494fb
Zack Kollar <rallokkcaz@gmail.com>
2022-03-18 17:50:27 -0700

    :notes: Pushing fixings and other thangs

--- src/components/nav-bar/nav-bar.tsx
+++ src/components/nav-bar/nav-bar.tsx
@@ -1,9 +1,10 @@
-import { Component, Host, Element, h, Prop, State, Watch } from '@stencil/core';
+import { Component, Host, Element, h, Prop, State, Watch, getAssetPath } from '@stencil/core';
 import { PlatformType } from '../../utils/platform';
 
 @Component({
   tag: 'nav-bar',
   styleUrl: 'nav-bar.css',
+  assetsDirs: ['assets'],
   shadow: true,
 })
 export class NavBar {
@@ -14,17 +15,21 @@ export class NavBar {
   @Prop() showMultiplier: number = 3;
 
   @State() deltaY: number = 0;
-  @State() currentHeight: number = 150;
   @State() scrollY: number = window.scrollY;
   @State() scrolled: boolean = false;
+  @State() scrollingUp: boolean = false;
+  @State() scrolledUp: number = 0;
+  @State() scrollThreshold: number = 500;
 
-  componentDidLoad() {
-    this.setHeight.bind(this);
-  }
+  private currentHeight: number = 150;
 
   componentWillLoad() {
     window.addEventListener('scroll', this.scrollListener.bind(this));
     window.addEventListener('resize', this.setHeight.bind(this));
+  }
+
+  componentDidLoad() {
+    this.setHeight();
   }
 
   disconnectedCallback() {
@@ -33,7 +38,7 @@ export class NavBar {
   }
 
   setHeight() {
-    this.currentHeight = this.el.getBoundingClientRect().height;
+    this.currentHeight = this.el.clientHeight;
   }
 
   scrollListener() {
@@ -41,49 +46,80 @@ export class NavBar {
   }
 
   @Watch('scrollY')
-  watchScrollY() {
-    // TODO: Fill me in!
+  watchScrollY(y, lastY) {
+    this.deltaY = y - lastY;
+
+    if (y < this.currentHeight * this.showMultiplier) {
+      this.scrolled = false;
+      // this.scrollingUp = false;
+      return;
+    } else {
+      this.scrolled = true;
+    }
+
+    if (this.deltaY < 0) {
+      if (this.scrolledUp++ >= this.scrollThreshold) {
+        this.scrollingUp = true;
+      }
+    } else if (this.deltaY > 0) {
+      this.scrollingUp = false;
+      this.scrolledUp = 0;
+    }
   }
 
   render() {
     return (
       <Host>
-        <nav class="navbar">
-          <div class="navbar__row">
-            <a href="#" class={'navbar__row__leading ' + (this.platform === 'blog' ? 'font-bold' : '')}>
-              Leibal Stories
-            </a>
-            <div class="navbar__row__links">
-              <a href="#">Architecture</a>
-              <a href="#">Interiors</a>
-              <a href="#">Furniture</a>
-              <a href="#">Lighting</a>
-              <a href="#">Products</a>
-              <a href="#">Travel</a>
+        <div style={{ height: (this.scrolled ? this.currentHeight : 0) + 'px' }}></div>
+        <animatable-component animation="slideInDown" easing="ease-in" duration={250}>
+          <nav class={'navbar ' + (this.scrolled ? 'navbar--scrolled ' : '') + (this.scrolledUp ? 'show' : '')}>
+            <div class="navbar__row">
+              <a
+                href="#"
+                class={
+                  'navbar__row__leading navbar__row__leading__logo ' + (this.platform === 'blog' ? 'font-bold' : '')
+                }
+              >
+                <div class="navbar__logo">
+                  <img src={getAssetPath(`./assets/leibal-logo.png`)} />
+                </div>
+                <div>Stories</div>
+              </a>
+              <div class="navbar__row__links">
+                <a href="#">Architecture</a>
+                <a href="#">Interiors</a>
+                <a href="#">Furniture</a>
+                <a href="#">Lighting</a>
+                <a href="#">Products</a>
+                <a href="#">Travel</a>
+              </div>
+              <div class="navbar__row__action">
+                <a href="#">Submit</a>
+                <a href="#">Sign Up</a>
+              </div>
             </div>
-            <div class="navbar__row__action">
-              <a href="#">Submit</a>
-              <a href="#">Sign Up</a>
+            <div class="navbar__row">
+              <a href="#" class={'navbar__row__leading ' + (this.platform === 'shop' ? 'font-bold' : '')}>
+                <div class="navbar__logo" style={{ opacity: '0', visibility: 'hidden' }}>
+                  <img src={getAssetPath(`./assets/leibal-logo.png`)} />
+                </div>
+                Shop
+              </a>
+              <div class="navbar__row__links">
+                <a href="#">Sofas</a>
+                <a href="#">Chairs</a>
+                <a href="#">Tables</a>
+                <a href="#">Storage</a>
+                <a href="#">Lighting</a>
+                <a href="#">Accessories</a>
+              </div>
+              <div class="navbar__row__action">
+                <a href="#">Trade</a>
+                <a href="#">Login</a>
+              </div>
             </div>
-          </div>
-          <div class="navbar__row">
-            <a href="#" class={'navbar__row__leading ' + (this.platform === 'shop' ? 'font-bold' : '')}>
-              Leibal Shop
-            </a>
-            <div class="navbar__row__links">
-              <a href="#">Sofas</a>
-              <a href="#">Chairs</a>
-              <a href="#">Tables</a>
-              <a href="#">Storage</a>
-              <a href="#">Lighting</a>
-              <a href="#">Accessories</a>
-            </div>
-            <div class="navbar__row__action">
-              <a href="#">Trade</a>
-              <a href="#">Login</a>
-            </div>
-          </div>
-        </nav>
+          </nav>
+        </animatable-component>
       </Host>
     );
   }
